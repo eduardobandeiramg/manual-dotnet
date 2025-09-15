ca8edcf59224c3f971c5bb36367701f562eeeb05
eduardobandeiramg <eduardobandeiramg@gmail.com>
2025-09-15 12:53:18 -0300

    incrementando estruturas de dados

--- code/05_estruturas_dados.cs
+++ code/05_estruturas_dados.cs
@@ -1,8 +1,13 @@
 class EstruturasDados
 {
+    // Arrays
+    static public int[] arrayInteiros = { 2, 4, 6, 8, 10 };
+    // arrays são muito limitados. Têm tamanho fixo definido no momento da inicialização e não é possível adicionar ou remover valores.
+    static public int[] arrayInteiros2 = [5]; // Pode-se atribuir valores depois. -> arrayInteiros[0] = 1; arrayInteiros[1] = 2 ...
+
     // Listas
-    static public List<string> listaSacolao = new List<string> () { "banana", "mamão", "uva" };
-    static public List<string> listaFarmacia = new List<string> ();
+    static public List<string> listaSacolao = new List<string>() { "banana", "mamão", "uva" };
+    static public List<string> listaFarmacia = new List<string>();
     List<dynamic> listaDinamica = new List<dynamic>();
     public static string[] listaDeStrings = { "primeiro", "segundo", "terceiro" };
 
@@ -11,7 +16,26 @@ class EstruturasDados
         listaFarmacia.Add(item);
     }
 
+    // Listas Encadeadas
+    LinkedList<int> listaEncadeada = new LinkedList<int>();
+
+    // Filas (FIFO)
+    public static Queue<int> fila = new Queue<int>();
+
+    // Pilhas (LIFO)
+    public static Stack<int> pilha = new Stack<int>();
+
     // Mapas/Dicionários
-    Dictionary<dynamic, dynamic> dicionarioVazio = new Dictionary<dynamic, dynamic>();
-    Dictionary<string, int> dicionarioInicializado = new Dictionary<string, int> () { {"Eduardo", 27}, {"Ernesto", 46} };
+    public static Dictionary<dynamic, dynamic> dicionarioVazio = new Dictionary<dynamic, dynamic>();
+    public static Dictionary<string, int> dicionarioInicializado = new Dictionary<string, int>() { { "Eduardo", 27 }, { "Ernesto", 46 } };
+
+    // Dicionários Ordenados (pela chave)
+    SortedDictionary<string, int> dicionarioOrdenado = new SortedDictionary<string, int>();
+    
+    // Conjuntos
+    HashSet<int> conjunto = new HashSet<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+    // Conjuntos Ordenados
+    SortedSet<int> conjuntoOrdenado = new SortedSet<int>() { 7, 8, 3, 4, 1, 9, 11 };
+
 }
--- code/Program.cs
+++ code/Program.cs
@@ -1,4 +1,32 @@
-﻿// Chamando a função com parâmetro de saída:
+﻿// 05 - Lidando com filas
+EstruturasDados.fila.Enqueue(1);
+EstruturasDados.fila.Enqueue(2);
+EstruturasDados.fila.Enqueue(3);
+EstruturasDados.fila.Dequeue(); // -> 2, 3
+foreach (int e in EstruturasDados.fila) {
+    System.Console.WriteLine(e);
+}
+
+// 05 - Lidando com pilhas
+EstruturasDados.pilha.Push(1);
+EstruturasDados.pilha.Push(2);
+EstruturasDados.pilha.Push(3);
+EstruturasDados.pilha.Pop(); // -> 2, 1
+foreach (int e in EstruturasDados.pilha)
+{
+    System.Console.WriteLine(e);
+}
+
+// 05 - Lidando com dicionários
+EstruturasDados.dicionarioInicializado.Add("Duba", 24);
+EstruturasDados.dicionarioInicializado["Joao"] = 22;
+EstruturasDados.dicionarioInicializado["Duba"] = 27;
+foreach (KeyValuePair<string, int> par in EstruturasDados.dicionarioInicializado) {
+    System.Console.WriteLine($"Chave: {par.Key}. Valor: {par.Value}");
+}
+
+
+// 04 - Chamando a função com parâmetro de saída:
 Funcoes objetoFuncoes = new Funcoes();
 bool resultado = objetoFuncoes.eParRetornaDobro(5, out int dobro);
 Console.WriteLine($"O valor retornado pela função é: {resultado} e o dobro é igual a {dobro}");
@@ -8,7 +36,7 @@ AprofundandoStrings.aprofundandoStrings();
 Console.WriteLine(EstruturasDados.listaDeStrings);
 
 
-// ** Lidando com classes e objetos **
+// 06 - ** Lidando com classes e objetos **
     // Instanciando classes com construtores definidos:
 Usuario usuario1 = new Usuario(nome: "Eduardo", id: "123", cpf: "123.456.789-00", dataNascimento: new DateTime(2000, 1, 1));
 Usuario2 usuario2 = new Usuario2(nome: "Eduardo", "123", "123.456.789-00", dataNascimento: new DateTime(2000, 1, 1));
